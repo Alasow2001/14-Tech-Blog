4c6538c8c87af4fa115c4597cf80215c5279358b
Ahmed Alasow <a.alasow01@gmail.com>
2023-03-01 12:28:47 +0000

    Changed database name

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    'crowdfund_db',
-    'root',
-    'S1nic_Wqe',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
